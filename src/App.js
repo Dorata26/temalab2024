b3337a4fbae219f0ae7f163ab50eb3ff9600eb88
Dorata26 <tamas.dori0426@gmail.com>
2024-04-12 22:32:44 +0200

    hosszu helynevek kb kijavitva

--- src/App.js
+++ src/App.js
@@ -1,58 +1,49 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const api = {
   key: '0cbcb3d7f93603d3da17737e0d766767',
   base: 'https://api.openweathermap.org/data/2.5/'
 };
 
+
 function App() {
+  
+
   const [search, setSearch] = useState('');
   const [result, setResult] = useState(null);
   const [animation, setAnimation] = useState('');
-  const locationRef = useRef(null); // Referencia objektum létrehozása
-
   useEffect(() => {
-    if (result) {
+    if(result){
       setTimeout(() => {
-        document.querySelector('.bottom').classList.add('visible');
-      }, 100);
-    }
-  }, [result]);
+        
+      document.querySelector('.bottom').classList.add('visible');},100);}
+    }, 
+    [result])
+    
+
 
   const searchPressed = () => {
-    if (search.trim() === '') {
+    
+    if(search.trim() === ''){
       alert('Please enter a location');
-      return;
-    }
+    return;}
     setResult(null);
     setAnimation(new Date().getTime().toString());
-
+   
     fetch(`${api.base}weather?q=${search}&APPID=${api.key}&units=metric`)
       .then(res => res.json())
-      .then(data => {
-        if (data.cod === '404') {
+      .then(data => {console.log(data);
+        //hibaüzenet ha nem találja meg a helyet
+        if(data.cod === '404'){
           alert('Location not found');
           return;
         }
         setResult(data);
         setSearch('');
         setAnimation(new Date().getTime().toString());
-        adjustFontSize();
+        
       })
       .catch(error => console.error('Error fetching data:', error));
-  };
-
-  const adjustFontSize = () => {
-    if (locationRef.current) {
-      const locationWidth = locationRef.current.offsetWidth;
-      const containerWidth = document.querySelector('.container').offsetWidth;
-      const locationText = result.name;
-      const fontSize = parseFloat(window.getComputedStyle(locationRef.current).fontSize);
-      if (locationWidth > containerWidth) {
-        const newFontSize = fontSize * (containerWidth / locationWidth);
-        locationRef.current.style.fontSize = `${newFontSize}px`;
-      }
-    }
   };
 
   return (
@@ -62,12 +53,11 @@ function App() {
           <div className='searchContainer'>
             <div className='search'>
               <input
+                
                 type='text'
                 placeholder='Enter location...'
                 onChange={e => setSearch(e.target.value)}
-                onKeyDown={e => {
-                  "Enter" === e.key && searchPressed();
-                }}
+                onKeyDown={e => {"Enter" === e.key && searchPressed()}}
                 value={search}
               />
               <button onClick={searchPressed}>Search</button>
@@ -77,50 +67,56 @@ function App() {
             {result && (
               <div>
                 <div className='location'>
-                  <h1 ref={locationRef}>{result.name}</h1>
+                  <h1>{result.name}</h1>
                 </div>
                 <div className='conutry'>
                   <h2>{result.sys.country}</h2>
                 </div>
                 <div className='temp'>
-                  <h1>{result.main.temp.toFixed(0)} C°</h1>
+                  <h1>{(result.main.temp).toFixed(0)} C°</h1>
                 </div>
                 <div className='description'>
                   <h3>{result.weather[0].description}</h3>
                 </div>
               </div>
             )}
-          </div>
+          </div>  
         </div>
-        {result && (
-          <div className='bottom'>
-            <div className='bottomBox'>
-              <p className='header'>Feels Like</p>
-              <p className='value'>{result.main.feels_like.toFixed(0)} C°</p>
+          {result && (
+            <div className='bottom'>
+              <div className='bottomBox'>
+                <p className='header'>Feels Like</p>
+                <p className='value'>{(result.main.feels_like).toFixed(0)} C°</p>
+              </div>
+              <div className='bottomBox'>
+                <p className='header'>Humidity</p>
+                <p className='value'>{result.main.humidity.toFixed(0)} %</p>
+              </div>
+              <div className='bottomBox'>
+                <p className='header'>Wind</p>
+                <p className='value'>{result.wind.speed.toFixed(1)} km/h</p>
+              </div>
+              <div className='bottomBox'>
+                <p className='header'>Min Max</p>
+                <p className='value'>
+                  {Math.floor(result.main.temp_min).toFixed(0)} - {Math.ceil(result.main.temp_max).toFixed(0)} C°
+                </p>
+              </div>
+              <div className='bottomBox'>
+                <p className='header'>Pressure</p>
+                <p className='value'>{result.main.pressure} mb</p>
+              </div>
             </div>
-            <div className='bottomBox'>
-              <p className='header'>Humidity</p>
-              <p className='value'>{result.main.humidity.toFixed(0)} %</p>
-            </div>
-            <div className='bottomBox'>
-              <p className='header'>Wind</p>
-              <p className='value'>{result.wind.speed.toFixed(1)} km/h</p>
-            </div>
-            <div className='bottomBox'>
-              <p className='header'>Min Max</p>
-              <p className='value'>
-                {Math.floor(result.main.temp_min).toFixed(0)} - {Math.ceil(result.main.temp_max).toFixed(0)} C°
-              </p>
-            </div>
-            <div className='bottomBox'>
-              <p className='header'>Pressure</p>
-              <p className='value'>{result.main.pressure} mb</p>
-            </div>
-          </div>
-        )}
+          )}
+        
       </div>
+      
     </div>
+    
   );
+  
 }
+
+
 
 export default App;
